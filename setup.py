2c02a30bc1bed11f682ea0cda3a0d9a790296908
edde746 <86283021+edde746@users.noreply.github.com>
2023-03-26 07:32:37 +0200

    PyPI Readme

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-# setup.py
 from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="random-ip-generator",
-    version="0.1.0",
+    version="0.1.1",
+    author="edde746",
+    description="A package to generate random IP addresses for a given country code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/edde746/random-ip-generator",
     packages=find_packages(),
     install_requires=[
         "ipaddress",
